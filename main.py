071bf4fbcabad42adea53884a45b979d004f91e1
reynaldo0 <reynaldooyusellino@gmail.com>
2024-06-23 07:23:42 +0700

    fix : move a code

--- main.py
+++ main.py
@@ -1,36 +1,6 @@
 import pygame
 import sys
 
-def ball_animation():
-    global ball_speed_x, ball_speed_y
-    
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-
-    if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:
-        ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= SCREEN_WIDTH:
-        ball_speed_x *= -1
-
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_speed_x *= -1
-
-def player_animation():
-    player.y += player_speed
-    if player.top <= 0:
-        player.top = 0
-    if player.bottom >= SCREEN_HEIGHT:
-        player.bottom = SCREEN_HEIGHT
-
-def opponent_ai():
-    if opponent.top < ball.y:
-        opponent.top += opponent_speed
-    if opponent.bottom > ball.y:
-        opponent.bottom -= opponent_speed
-    if opponent.top <= 0:
-        opponent.top = 0
-    if opponent.bottom >= SCREEN_HEIGHT:
-        opponent.bottom = SCREEN_HEIGHT
 # General setup
 pygame.init()
 clock = pygame.time.Clock()
@@ -56,6 +26,37 @@ ball_speed_y = 7
 player_speed = 0
 opponent_speed = 7
 
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= SCREEN_WIDTH:
+        ball_restart()
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+def player_animation():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= SCREEN_HEIGHT:
+        player.bottom = SCREEN_HEIGHT
+
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= SCREEN_HEIGHT:
+        opponent.bottom = SCREEN_HEIGHT
+        
 run = True
 while run:
     for event in pygame.event.get():
